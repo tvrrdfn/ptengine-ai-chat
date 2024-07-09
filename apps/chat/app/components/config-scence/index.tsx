3045e3f94a98d71acf0a50f900797e2679ab404f
ao <dawn@ptmind.com>
2024-07-09 12:10:42 +0800

    feat: update style

--- apps/chat/app/components/config-scence/index.tsx
+++ apps/chat/app/components/config-scence/index.tsx
@@ -3,14 +3,12 @@ import React from 'react'
 import type { IWelcomeProps } from '../welcome'
 import Welcome from '../welcome'
 import Home from '../home'
-import ChatInput from '../chat-input'
 
 const ConfigSence: FC<IWelcomeProps> = (props) => {
     return (
         <div className='mb-5 antialiased font-sans overflow-hidden shrink-0'>
             {!props.hasSetInputs && <Home {...props} />}
-            {/* <Welcome {...props} /> */}
-            {<ChatInput {...props} />}
+            <Welcome {...props} />
         </div>
     )
 }
